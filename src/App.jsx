3396c596074ba6a47dc396088948f4b8ec96a073
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-11-10 16:47:29 +0100

    commenting out routes that aren't configured yet to test the pageloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import AppLoaderSplash from "./components/appLoaderSplash/AppLoaderSplash.jsx";
 import {Routes, Route} from 'react-router-dom';
-import Home from "./pages/homepage/Home.jsx";
+// import Home from "./pages/homepage/Home.jsx";
 import Register from "./components/register/Register.jsx"
 import AuthNav from "./components/authNavigation/AuthNav.jsx";
-import UserNav from "./components/userNavigation/UserNav.jsx"
+// import UserNav from "./components/userNavigation/UserNav.jsx"
 import Login from "./components/login/Login.jsx";
 // import {AuthContext} from "./context/AuthContext.jsx";
 // import {useContext} from "react";
-import UserDetails from "./pages/userDetailsPage/UserDetailsPage.jsx";
 
 function App() {
     // const { isAuth } = useContext(AuthContext);
@@ -18,13 +17,13 @@ function App() {
             <AppLoaderSplash />
 
             <AuthNav/>
-            <UserNav/>
+            {/*<UserNav/>*/}
 
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                {/*<Route path="/" element={<Home/>}/>*/}
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/signin" element={<Login/>}/>
-                <Route path="app/users/:username" element={<UserDetails/>} />
+                {/*<Route path="app/users/:username" element={<UserDetails/>} />*/}
                 {/*<Route path="/admin/dashboard" element={<Dashboard />}/>*/}
 
                 {/*<Route*/}
